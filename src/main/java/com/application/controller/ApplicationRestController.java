05ce8d1cbcd950d17c9704b2d9efecfec275d45f
bapostol91 <bapostol91@yahoo.com>
2018-07-06 20:59:01 +0300

    logger

--- src/main/java/com/application/controller/ApplicationRestController.java
+++ src/main/java/com/application/controller/ApplicationRestController.java
@@ -2,6 +2,7 @@ package com.application.controller;
 
 import com.application.dto.ApplicationDTO;
 import com.application.service.ApplicationService;
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +11,13 @@ import java.util.List;
 
 @RestController
 public class ApplicationRestController {
-
+    Logger LOG = Logger.getLogger(ApplicationRestController.class);
     @Resource
     private ApplicationService applicationService;
 
     @RequestMapping("/applications")
     public List<ApplicationDTO> getApplications() {
+        LOG.info("==============> getApplications called!");
         return applicationService.getApplications();
     }
 }
