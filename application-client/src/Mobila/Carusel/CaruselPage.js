beca76d9215197392e2e1cfc4b805c40b517d66a
Bogdan Apostol <Bogdan.Apostol@endava.com>
2018-10-04 12:56:39 +0300

    full screen carousel

--- application-client/src/Mobila/Carusel/CaruselPage.js
+++ application-client/src/Mobila/Carusel/CaruselPage.js
@@ -1,11 +1,4 @@
 import React from "react";
 import Carusel from "./Carusel";
-import Page from "../Page/Page";
 
-export default (images) => {
-	return (
-		<Page>
-			<Carusel images={images}/>
-		</Page>
-	);
-}
+export default (images) =>  <Carusel images={images}/>;
